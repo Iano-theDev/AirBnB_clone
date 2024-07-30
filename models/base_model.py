d48224d525a71ffdfa1a2eb39a95432024619048
Iano-theDev <sinceianmike@gmail.com>
2024-07-31 00:58:55 +0300

    use args and kwargs to instantiate BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,15 @@ from datetime import datetime
 
 class BaseModel:
     """Represents the base model of the air-bnb storage model"""
-    id: str
-    # created_at
-
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if not kwargs: 
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """returns human readable representation of the BaseModel class"""
@@ -54,3 +57,4 @@ class BaseModel:
 # print(type(d_json['created_at']))
 # print(type(d_json['__class__']))
 # print(d_json['__class__'])
+
