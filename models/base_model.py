40a2489cbe539321831d0c118f1341365dd8b86e
Iano-theDev <sinceianmike@gmail.com>
2024-08-14 01:16:24 +0300

    Add file storage engine

--- models/base_model.py
+++ models/base_model.py
@@ -3,19 +3,24 @@ The base model class module
 """
 import uuid
 from datetime import datetime
+import models
+# from models import storage
+fmt = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
     """Represents the base model of the air-bnb storage model"""
     def __init__(self, *args, **kwargs):
-        if not kwargs: 
+        if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
+            models.storage.save()
         else:
             kwargs["id"] = str(kwargs['id'])
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'], fmt)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'], fmt)
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -25,16 +30,16 @@ class BaseModel:
     def save(self):
         """updates the public instance attribute updated_at"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """returns a dictionary containing all keys/values of __dict__ of the BaseModel instance"""
-        bmDict = self.__dict__
+        """returns a dictionary containing all keys/values of __dict__
+        of the BaseModel instance"""
+        bmDict = self.__dict__.copy()
         bmDict['__class__'] = self.__class__.__name__
         bmDict["created_at"] = bmDict["created_at"].isoformat()
-        if ('updated_at' in bmDict):
-            bmDict["updated_at"] = bmDict["updated_at"].isoformat()
+        bmDict["updated_at"] = bmDict["updated_at"].isoformat()
         return bmDict
-
 
 
 # newObj = BaseModel()
@@ -58,4 +63,3 @@ class BaseModel:
 # print(type(d_json['created_at']))
 # print(type(d_json['__class__']))
 # print(d_json['__class__'])
-
---
+++ models/engine/file_storage.py
@@ -0,0 +1,49 @@
+"""The file storage module
+"""
+import json
+import os
+from models.base_model import BaseModel
+
+classes = {"BaseModel": BaseModel}
+
+
+class FileStorage:
+    """The file starage class
+    handles sotorage of files to json and reading from json to dict"""
+    __file_path: str = "file.json"
+    __objects: dict = {}
+
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """sets in __objects to obj and with with obj classname.id"""
+        if obj is not None:
+            className = obj.__class__.__name__
+            id = obj.id
+            key = str(className) + "." + str(id)
+            self.__objects[key] = obj
+
+    def save(self):
+        """serializes __objjects to the JSON file (path) __file_path"""
+        # print(f'[__objects is]:{self.__objects}')
+        dict_to_save = {}
+        for key, value in self.__objects.items():
+            dict_to_save[key] = value.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(dict_to_save, file)
+
+    def reload(self):
+        """deserializes tjson file to __objects only if the JSON file exists.
+        No exception is raised if the file doesn't exist"""
+        PATH = self.__file_path
+        loaded_dict = {}
+        if os.path.exists(PATH):
+            with open(PATH, 'r') as file:
+                loaded_dict = json.load(file)
+            for key, value in loaded_dict.items():
+                objClass = classes[value["__class__"]]
+                self.__objects[key] = objClass(**value)
+        else:
+            pass
